c2c8caac55efae8a140b2ceb8fa9f58eeabbd7eb
dmolio <dmolio@molnafsystems.com>
2024-10-28 19:51:38 -0400

    updated cicd

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: './src/test/setup.ts',
+    css: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
